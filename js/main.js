d1a258ced321945a2048681b9e34e0dea856f21c
Юлия <Iulia.Dediukhina@urfu.me>
2022-09-19 12:23:14 +0500

    Переписаны вспомогательные функции для дальнейшей работы

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 //1. Возвращение случайного целого числа
-function getRandomInt(from, to) {
-  const random = Math.random() * (to + 1 - from) + from;
-  if (random >= 0) {
-    return ('Запись некорректна, так как диапазон может быть только положительным');
-  } else {
-    if (from >= to){
-      return('Проверьте не равны ли значения, которые Вы ввели и запишите их в порядке возрастания');
-    }
-  } return Math.floor(random);
-}
+const getRandomInt = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+  if (min > max){
+    [min, max] = [max, min];
+  }
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+};
 
 //2. Проверка максимальной длины строки
-function stringLength(checkingString, maxLength) {
-  if (checkingString <= maxLength) {
-    return true;
-  } return false;
-}
+const checkStringLength = (str, max = 100) => str.length <= max;
