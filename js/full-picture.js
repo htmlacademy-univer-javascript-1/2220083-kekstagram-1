f8ff0cc988aba1666f23f4a0acefe479f81d6be2
Yulia03De <Iulia.Dediukhina@urfu.me>
2022-11-02 14:29:55 +0500

    Реализован сценарий просмотра фотографий в полноразмерном режиме

--- js/full-picture.js
+++ js/full-picture.js
@@ -1,28 +1,48 @@
-import { photos } from './data.js';
+//import { photos } from './data.js';
+import { createComment } from './comments.js';
+
+//const bigPicture = document.querySelector('.big-picture');
+//const closeButton = document.querySelector('.big-picture__cancel');
+//const pictures = document.querySelectorAll('.picture');
 
 const bigPicture = document.querySelector('.big-picture');
-const closeButton = document.querySelector('.big-picture__cancel');
-const pictures = document.querySelectorAll('.picture');
+const commentCounter = document.querySelector('.social__comment-count');
+const commentLoader = document.querySelector('.comments-loader');
+const closeButton = bigPicture.querySelector('.big-picture__cancel');
+const commentTemplate = bigPicture.querySelector('.social__comment');
+
+const onDocumentEscKeyDown = (evt) => {
+  if(evt.key === 'Escape'){
+    bigPicture.classList.add('hidden');
+    document.querySelector('body').classList.remove('modal-open');
+
+    document.removeEventListener('keydown', onDocumentEscKeyDown);
+  }
+};
 
 const addPictureEvenHandler = (picture, pictureData) =>{
   picture.addEventListener('click', () => {
     bigPicture.classList.remove('hidden');
 
     bigPicture.querySelector('.big-picture__img').querySelector('img').src = pictureData.url;
-
-    const numberLikes = bigPicture.querySelector('.big-picture__social');
-    numberLikes.querySelector('.likes-count').textContent = pictureData.likes;
-
-    const commentsCount = bigPicture.querySelector('.social__comment-count');
-    commentsCount.querySelector('.comments-count').textContent = pictureData.comments.length;
-
+    bigPicture.querySelector('.likes-count').textContent = pictureData.likes;
+    bigPicture.querySelector('.comments-count').textContent = pictureData.comments.length;
     bigPicture.querySelector('.social__caption').textContent = pictureData.description;
 
+    //const numberLikes = bigPicture.querySelector('.big-picture__social');
+    //numberLikes.querySelector('.likes-count').textContent = pictureData.likes;
+
+    //const commentsCount = bigPicture.querySelector('.social__comment-count');
+    //commentsCount.querySelector('.comments-count').textContent = pictureData.comments.length;
+
+    //bigPicture.querySelector('.social__caption').textContent = pictureData.description;
+
     const comments = bigPicture.querySelector('.social__comments');
-    const commentCopy = comments.querySelector('li');
+    //const commentCopy = comments.querySelector('li');
 
     comments.innerHTML = '';
 
+    /*
     const addComments = () => {
       for(let i = 0; i < pictureData.comments.length; i++){
         const newItem = commentCopy.cloneNode(true);
@@ -34,11 +54,24 @@ const addPictureEvenHandler = (picture, pictureData) =>{
         comments.appendChild(newItem);
       }
     };
+    */
 
+    /*
     addComments();
     document.querySelector('.social__comment-count').classList.add('hidden');
     document.querySelector('.comments-loader').classList.add('hidden');
     document.querySelector('body').classList.add('modal-open');
+    */
+
+    pictureData.comments.forEach((comment) => {
+      comments.appendChild(createComment(comment, commentTemplate));
+    });
+
+    commentCounter.classList.add('hidden');
+    commentLoader.classList.add('hidden');
+    document.body.classList.add('modal-open');
+
+    document.addEventListener('keydown', onDocumentEscKeyDown);
   });
 };
 
@@ -47,15 +80,9 @@ closeButton.addEventListener('click', ()=> {
   document.querySelector('body').classList.remove('modal-open');
 });
 
-const onDocumentEscKeyDown = (evt) => {
-  if(evt.key === 'Escape'){
-    bigPicture.classList.add('hidden');
-    document.querySelector('body').classList.remove('modal-open');
+export{addPictureEvenHandler};
 
-    document.removeEventListener('keydown', onDocumentEscKeyDown);
-  }
-};
-
+/*
 document.addEventListener('keydown', onDocumentEscKeyDown);
 
 const makeBigPictures = () =>{
@@ -65,3 +92,4 @@ const makeBigPictures = () =>{
 };
 
 makeBigPictures();
+*/
