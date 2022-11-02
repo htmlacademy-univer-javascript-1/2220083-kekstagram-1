111a4e998266e5f3d58601e34c1c8425b49b2317
Yulia03De <Iulia.Dediukhina@urfu.me>
2022-11-02 14:37:15 +0500

    Реализован сценарий просмотра фотографий в полноразмерном режиме

--- js/full-picture.js
+++ js/full-picture.js
@@ -1,9 +1,4 @@
-//import { photos } from './data.js';
 import { createComment } from './comments.js';
-
-//const bigPicture = document.querySelector('.big-picture');
-//const closeButton = document.querySelector('.big-picture__cancel');
-//const pictures = document.querySelectorAll('.picture');
 
 const bigPicture = document.querySelector('.big-picture');
 const commentCounter = document.querySelector('.social__comment-count');
@@ -29,39 +24,9 @@ const addPictureEvenHandler = (picture, pictureData) =>{
     bigPicture.querySelector('.comments-count').textContent = pictureData.comments.length;
     bigPicture.querySelector('.social__caption').textContent = pictureData.description;
 
-    //const numberLikes = bigPicture.querySelector('.big-picture__social');
-    //numberLikes.querySelector('.likes-count').textContent = pictureData.likes;
-
-    //const commentsCount = bigPicture.querySelector('.social__comment-count');
-    //commentsCount.querySelector('.comments-count').textContent = pictureData.comments.length;
-
-    //bigPicture.querySelector('.social__caption').textContent = pictureData.description;
-
     const comments = bigPicture.querySelector('.social__comments');
-    //const commentCopy = comments.querySelector('li');
 
     comments.innerHTML = '';
-
-    /*
-    const addComments = () => {
-      for(let i = 0; i < pictureData.comments.length; i++){
-        const newItem = commentCopy.cloneNode(true);
-
-        newItem.querySelector('p').textContent = pictureData.comments[i]['message'];
-        newItem.querySelector('img').src = pictureData.comments[i]['avatar'];
-        newItem.querySelector('img').alt= pictureData.comments[i]['name'];
-
-        comments.appendChild(newItem);
-      }
-    };
-    */
-
-    /*
-    addComments();
-    document.querySelector('.social__comment-count').classList.add('hidden');
-    document.querySelector('.comments-loader').classList.add('hidden');
-    document.querySelector('body').classList.add('modal-open');
-    */
 
     pictureData.comments.forEach((comment) => {
       comments.appendChild(createComment(comment, commentTemplate));
@@ -81,15 +46,3 @@ closeButton.addEventListener('click', ()=> {
 });
 
 export{addPictureEvenHandler};
-
-/*
-document.addEventListener('keydown', onDocumentEscKeyDown);
-
-const makeBigPictures = () =>{
-  for(let i = 0; i < pictures.length; i++){
-    addPictureEvenHandler(pictures[i],photos[i]);
-  }
-};
-
-makeBigPictures();
-*/
