4c6097c8b1cd5f5b88d55ec6befb35723c455e5c
Keks <keks@htmlacademy.ru>
2022-11-22 12:29:21 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,4 @@
+import './pictures.js';
+import './full-picture.js';
+import './upload.js';
+import './hashtags-pristine.js';
---
+++ 7/js/util.js
@@ -0,0 +1,13 @@
+const getRandomInteger = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+  if (min > max){
+    [min, max] = [max, min];
+  }
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+};
+
+const isEscape = (evt) => evt.key === 'Escape';
+
+export{getRandomInteger, isEscape};
