6f7a50cc4e89237cffe7e9d89209878f92c02fc6
Keks <keks@htmlacademy.ru>
2022-12-12 09:14:53 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,4 @@
+import './upload.js';
+import './pictures.js';
+import './full-picture.js';
+import './hashtags-pristine.js';
---
+++ 8/js/pictures.js
@@ -0,0 +1,38 @@
+import { photos } from './data.js';
+import { getFullPictures } from './full-picture.js';
+
+const picture = document.querySelector('.pictures');
+const template = document.querySelector('#picture').content;
+const templatePictures = template.querySelector('.picture');
+
+const newFragment = document.createDocumentFragment();
+
+const renderPhoto = (photo) => {
+  const item = templatePictures.cloneNode(true);
+
+  const img = item.querySelector('.picture__img');
+  img.src = photo.url;
+
+  const likes = item.querySelector('.picture__likes');
+  likes.textContent = photo.likes;
+
+  const comments = item.querySelector('.picture__comments');
+  comments.textContent = photo.comments.length;
+
+  item.addEventListener('click', (evt) =>{
+    evt.preventDefault();
+    getFullPictures(photo);
+  });
+
+  return item;
+};
+
+const renderPhotos = () => {
+  photos.forEach((photo) => {
+    newFragment.appendChild(renderPhoto(photo));
+  });
+
+  picture.appendChild(newFragment);
+};
+
+renderPhotos();
